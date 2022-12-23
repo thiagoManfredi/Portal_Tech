0490a509b53cfd498950c16ae03393f8e088f10a
Thiago Manfredi <thi.manf@gmail.com>
2022-12-23 17:41:44 -0300

    Pasta JavaScript2 criada. Primeira atividade adicionada.

---
+++ 06)_JavaScript2/01_form-style-classList/README.md
@@ -0,0 +1,12 @@
+<h3>Feito em aula (22/12/2022)</h3>
+
+- Estilização do css por meio da propriedade ClassList;
+- Estruturas condicionais e funções em JavaScript;
+- Chamada de uma função através do evento Onclick em um botão.
+
+<h3>GitHub Pages:</h3>
+Link???
+
+<h3>Preview:</h3>
+![Preview](https://user-images.githubusercontent.com/118065155/209403663-364e8fdd-a6d3-469d-97d4-de4105e37c64.png)
+
---
+++ 06)_JavaScript2/01_form-style-classList/script.js
@@ -0,0 +1,29 @@
+const title = document.getElementsByClassName("title")
+const error_text = document.getElementsByClassName('error-text')
+const username_input = document.getElementById('login-usuario')
+const password_input = document.getElementById('login-senha')
+const success_message = document.getElementsByClassName('success-message')
+
+// console.log(success_message)
+
+function validateForms() {
+    if (username_input.value != 'Thiago') {
+        error_text[0].classList.add('visible')
+    } else {
+        error_text[0].classList.remove('visible')        
+    }
+    if (password_input.value != '123') {
+        error_text[1].classList.add('visible')
+    } else {
+        error_text[1].classList.remove('visible')
+    }
+}
+
+validateForms()
+
+function showSuccessMessage() {
+    if (username_input.value == 'Thiago' && password_input.value == '123') {
+        success_message[0].classList.add('visible')
+    }
+}
+
