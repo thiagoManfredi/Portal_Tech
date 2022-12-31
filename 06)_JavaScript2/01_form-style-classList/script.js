2dc7083f8aed7b7e801a0179617fb2ddc6ea774e
Thiago Manfredi <thi.manf@gmail.com>
2022-12-31 14:45:29 -0300

    Mensagem de erro incluída.

--- 06)_JavaScript2/01_form-style-classList/script.js
+++ 06)_JavaScript2/01_form-style-classList/script.js
@@ -3,8 +3,7 @@ const error_text = document.getElementsByClassName('error-text')
 const username_input = document.getElementById('login-usuario')
 const password_input = document.getElementById('login-senha')
 const success_message = document.getElementsByClassName('success-message')
-
-// console.log(success_message)
+const fail_message = document.getElementsByClassName('fail-message')
 
 function validateForms() {
     if (username_input.value != 'Thiago') {
@@ -24,6 +23,10 @@ validateForms()
 function showSuccessMessage() {
     if (username_input.value == 'Thiago' && password_input.value == '123') {
         success_message[0].classList.add('visible')
+        fail_message[0].classList.remove('visible')
+    } else {
+        success_message[0].classList.remove('visible')
+        fail_message[0].classList.add('visible')        
     }
 }
 
